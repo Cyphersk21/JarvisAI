c54e69299734fffcaea6e2995dab7f66cc1f279d
Satishkumar Kamble <118530399+Cyphersk21@users.noreply.github.com>
2023-10-17 23:06:22 +0530

    openaitest.py

--- openaitest.py
+++ openaitest.py
@@ -1,20 +1,3 @@
-# import os
-# import openai
-#
-# from config import apikey
-#
-# openai.api_key = apikey
-#
-# response = openai.Completion.create(
-#   model="gpt-3.5-turbo-instruct",
-#   prompt="Write an email application to company for software developer fresher position",
-#   temperature=1,
-#   max_tokens=256,
-#   top_p=1,
-#   frequency_penalty=0,
-#   presence_penalty=0
-# )
-# print(response)
 import os
 import openai
 from config import ChatGPT_apikey
